d32d182dc4e0676a8858cf6bdef9dbf15d1e5614
Abhishek Parmar <abhishek205parmar@gmail.com>
2026-01-19 15:25:52 +0530

    [Abhishek] Submitted

---
+++ Q2/Custom_module.js
@@ -0,0 +1,5 @@
+const stringUtils = require("./StringUtils");
+
+console.log(stringUtils.capitalize("Abhishek"));      
+console.log(stringUtils.reverseString("hello"));   
+console.log(stringUtils.countVowels("Ankit Verma"));
