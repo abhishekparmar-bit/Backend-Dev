75928bc4143ccccec90c2179c8d15009848777de
Abhishek Parmar <abhishek205parmar@gmail.com>
2026-02-11 15:09:07 +0530

    [Abhishek] Submitted on 11-02-2025

---
+++ Express-backend/public/script.js
@@ -0,0 +1,23 @@
+const express=require("express")
+
+const app=express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+
+// To serve static files
+// app.use(express.static("public"))
+
+const PORT=5000;
+
+app.get("/register",(req,res)=>{
+    res.sendFile(__dirname+"/form.html");
+})
+
+app.post("/student/register",(req,res)=>{
+    console.log("form data:",req.body);
+    res.send("register");
+})
+app.listen(PORT,()=>{
+    console.log(`Server is running on: ${PORT}`)
+})
