8db3496f1593bd60755f3dfd263c9f0f62ab218b
Abhishek Parmar <abhishek205parmar@gmail.com>
2026-02-11 15:22:24 +0530

    final save before rebase

--- Express-backend/public/index.js
+++ Express-backend/public/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express=require("express")
 
 const app=express();
@@ -21,3 +22,28 @@ app.post("/student/register",(req,res)=>{
 app.listen(PORT,()=>{
     console.log(`Server is running on: ${PORT}`)
 })
+=======
+const express=require("express")
+
+const app=express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+
+// To serve static files
+// app.use(express.static("public"))
+
+const PORT=5000;
+
+app.get("/register",(req,res)=>{
+    res.sendFile(__dirname+"/form.html");
+})
+
+app.post("/student/register",(req,res)=>{
+    console.log("form data:",req.body);
+    res.send("register");
+})
+app.listen(PORT,()=>{
+    console.log(`Server is running on: ${PORT}`)
+})
+>>>>>>> 2b26138 ([Abhishek] Submitted on 11-02-2025)
